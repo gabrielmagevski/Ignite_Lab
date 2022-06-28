7b79fe2226b72adf106e4aad97ae37a9bca25c2a
gabrielmagevski <gabrielmagevski.gs@gmail.com>
2022-06-27 21:02:00 -0300

    segunda-aula finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import { gql, useQuery } from '@apollo/client';
 import './styles/global.css';
-
-const SearchLessons = gql`
-  query {
-    lessons {
-      id,
-      slug,
-      title,
-      teacher {
-        avatarURL,
-        bio,
-        name
-      }
-    }
-  }
-`
-
-type Lesson = {
-  id: string;
-  title: string;
-}
+import { Event } from './pages/Event'
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(SearchLessons);
-  console.log(data);
 
   return (
-   <ul>
-      {
-        data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-        })
-      }
-   </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
